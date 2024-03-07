b9de5cd24c12fd5037e27aa70586185e7234ed60
sagar9951 <skarmakar1861995@gmail.com>
2024-03-07 12:17:14 +0530

    teseter committed the files

---
+++ src/test/java/tester03/TesterClass.java
@@ -0,0 +1,11 @@
+package tester03;
+
+import org.testng.annotations.Test;
+
+public class TesterClass {
+
+	@Test
+	public void thirdMethod() {
+		System.out.println("---third print statatement");
+	}
+}
